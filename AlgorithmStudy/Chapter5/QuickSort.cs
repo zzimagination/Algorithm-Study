479de2561194dc9f245d477dd02d7463574ed7e5
semper <zhaoimag@163.com>
2018-07-17 20:19:46 +0800

    717修改

---
+++ AlgorithmStudy/Chapter5/QuickSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmStudy.Chapter5
+{
+    class QuickSort
+    {
+
+        /// <summary>
+        /// 快速排序法
+        /// 输入数组a和相对子数组左下标l，右下标r
+        /// 输出非降序数组
+        /// </summary>
+        /// <param name="arrays"></param>
+        /// <param name="l"></param>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static int[] Sort(int[] arrays,int l, int r)
+        {
+            if (l<r)
+            {
+                int s = HoarePartition(arrays,l,r);
+                
+                Sort(arrays,l,s-1);
+                Sort(arrays,s+1,r);
+
+             
+            }
+            return arrays;
+        }
+
+        /// <summary>
+        /// 以第一个元素为中轴对子数组进行划分
+        /// 输入数组a[0,n-1]
+        /// 输出a的一个划分，分裂点为返回值
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int HoarePartition(int[] numbers,int l, int r)
+        {
+            int p = numbers[l];
+            int i = l+1;
+            int j = r;
+            while (i < j)
+            {
+                while (numbers[i] < p)
+                {
+                    i += 1;
+                    if (i > r)
+                        break;
+                }
+                while (numbers[j] > p)
+                {
+                    j -= 1;
+                }
+                numbers[i] = numbers[i] + numbers[j];
+                numbers[j] = numbers[i] - numbers[j];
+                numbers[i] = numbers[i] - numbers[j];
+
+            }
+            numbers[i] = numbers[i] + numbers[j];
+            numbers[j] = numbers[i] - numbers[j];
+            numbers[i] = numbers[i] - numbers[j];
+
+            numbers[l] = numbers[l] + numbers[j];
+            numbers[j] = numbers[l] - numbers[j];
+            numbers[l] = numbers[l] - numbers[j];
+
+            return j;
+        }
+
+    }
+}
--- AlgorithmStudy/Program.cs
+++ AlgorithmStudy/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using static System.Threading.Thread;
+using System.Threading;
 
 namespace AlgorithmStudy
 {
@@ -29,13 +31,47 @@ namespace AlgorithmStudy
 
             //Console.WriteLine( Chapter4.BinarySearchUtility.BinarySearch(nums,89));
 
-            int[] nums = { 8, 3, 2, 9, 7, 1, 5, 4 };
-            int[] numF= Chapter5.MergingSort.Sort(nums);
-            foreach (var item in numF)
-            {
-                Console.WriteLine(item);
-            }
+            //int[] nums = { 8, 3, 2, 9, 7, 1, 5, 4 };
+            //int[] numF= Chapter5.QuickSort.Sort(nums,0,7);
+            //foreach (var item in numF)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Thread tr1 = new Thread(ThreadStudy);
+            tr1.Priority = ThreadPriority.Lowest;
+            Thread tr2 = new Thread(ThreadStudy2);
+            tr2.Priority = ThreadPriority.Highest;
+            tr1.Start();
+            tr2.Start();
+            start = true;
+            Sleep(TimeSpan.FromSeconds(4));
+            start = false;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
             Console.ReadLine();
         }
+
+        static int num1;
+        static int num2;
+        static bool start;
+        static void ThreadStudy()
+        {
+
+            while(start)
+            {
+                num1++;
+                
+            }
+        }
+
+        static void ThreadStudy2()
+        {
+            while(start)
+            {
+                num2++; 
+            }
+        }
+
     }
 }
